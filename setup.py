8e709d4fd96f56f94c5f9d8b853697b0b26fe4c8
resteve <resteve@zikzakmedia.com>
2013-11-13 15:19:11 +0100

    Setup

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ tests_require = ['proteus >= %s.%s, < %s.%s' %
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='',
+    description='Tryton module stock scanner lot',
     long_description=read('README'),
     author='NaN·tic',
     url='http://www.nan-tic.com/',
