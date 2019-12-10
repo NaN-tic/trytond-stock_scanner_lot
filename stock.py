4be0b6755ae98047bba6a0de4943afc07134d61d
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2019-12-10 16:44:48 +0100

    Use direct access to backend classes.

--- stock.py
+++ stock.py
@@ -54,8 +54,7 @@ class ConfigurationScannerLotCreation(ModelSQL, CompanyValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(ConfigurationScannerLotCreation, cls).__register__(module_name)
 
