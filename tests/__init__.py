22d462f12c132ee09bc51bdcf5a6131503dcaea6
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:12:35 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-
-from .test_stock_scanner_lot import suite
-
-__all__ = ['suite']
